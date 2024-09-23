eaa53c6b1ba406979ae62fce2de4ba0034a5a5d3
Pritam Kundu <pritam.kundu144@gmail.com>
2024-09-23 16:02:13 +0000

    Bianry Search

--- BinarySearch/OrderAgnosticBinarySearch.java
+++ BinarySearch/OrderAgnosticBinarySearch.java
@@ -1,12 +1,12 @@
-// /*
-//  * In order agnostic binary search - we don't know whether the array is sorted in ascending or descending 
-//  * order but we know that the array is sorted so first we have to check whether the array is in ascending or
-//  * descending order.
-//  * 
-//  * Suppose an array given = [3,3,3,3,3,3,87]
-//  * We can check the first element and last element and then we can easily make an decsiion whether the
-//  * array is sorted in ascending order or descending order
-//  */
+/*
+ * In order agnostic binary search - we don't know whether the array is sorted in ascending or descending 
+ * order but we know that the array is sorted so first we have to check whether the array is in ascending or
+ * descending order.
+ * 
+ * Suppose an array given = [3,3,3,3,3,3,87]
+ * We can check the first element and last element and then we can easily make an decsiion whether the
+ * array is sorted in ascending order or descending order
+ */
 
 public class OrderAgnosticBinarySearch {
     public static void main(String[] args) {
